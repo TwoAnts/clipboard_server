0a500c6e48c3789c4132b240afa9468a5384c7d3
TwoAnts <lzmyhzy@gmail.com>
2023-09-07 22:40:46 +0800

    Support build docker image

---
+++ docker/build_img.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+rm -rf build
+mkdir -p build
+
+tar -C .. --exclude-vcs --exclude=docker -cf build/clipboard_server.tar .
+gzip build/clipboard_server.tar
+
+#docker build
+docker build -t clipboard_server:v1 .
+
